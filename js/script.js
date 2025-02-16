393739d71b0996e120a186dc1cc5447b6522f225
Aroldo68 <k21ajo@gmail.com>
2025-02-15 22:22:49 -0300

    Complementar Pagina Inicial

--- js/script.js
+++ js/script.js
@@ -9,6 +9,75 @@ const sectionCarrinho = document.querySelector('.carrinho')
 let usuarioLogado = false
 
 //Navegação
+const ocultarElemento = (elemento) => {
+    elemento.style.display = 'none'
+}
+
+const mostrarElemento = (elemento, display='block') => {
+    elemento.style.display = display
+}
+
+const irParaHome = () => {
+    ocultarElemento(sectionPagamento)
+    ocultarElemento(sectionIdentificacao)
+    ocultarElemento(sectionIdentifiquese)
+    ocultarElemento(sectionCarrinho)
+    ocultarElemento(botaoVoltar)
+    ocultarElemento(sectionDetalhesProduto)
+    mostrarElemento(sectionHero, 'flex')
+    mostrarElemento(sectionProdutos, 'flex')
+}
+
+const irParaPagamento = () => {
+    ocultarElemento(sectionIdentifiquese)
+    if(numeroItens.innerHTML > 0) {
+        ocultarElemento(sectionHero)
+        ocultarElemento(sectionProdutos)
+        ocultarVoltarEsecaoDetalhes()
+        ocultarElemento(sectionCarrinho)
+        mostrarElemento(sectionPagamento)
+    }
+}
+
+const ocultarVoltarEsecaoDetalhes = () => {
+    ocultarElemento(botaoVoltar)
+    ocultarElemento(sectionDetalhesProduto)
+}
+ocultarVoltarEsecaoDetalhes()
+
+botaoVoltar.addEventListener('click', () => {
+    mostrarElemento(sectionProdutos, 'flex')
+    ocultarVoltarEsecaoDetalhes()
+})
+
+const btnCarrinho = document.querySelector('.btn__carrinho .icone')
+btnCarrinho.addEventListener('click', () => {
+    if(numeroItens.innerHTML > 0) {
+        mostrarElemento(sectionCarrinho)
+        ocultarElemento(sectionHero)
+        ocultarElemento(sectionProdutos)
+        ocultarElemento(sectionDetalhesProduto)
+        ocultarElemento(sectionIdentificacao)
+        ocultarElemento(sectionPagamento)
+    }
+})
+
+const btnHome = document.querySelector('.link_home')
+btnHome.addEventListener('click', (event) => {
+    event.preventDefault()
+    irParaHome()
+})
+
+// NUMERO DE ITENS do CARRINHO
+const numeroItens = document.querySelector('.numero_itens')
+ocultarElemento(numeroItens)
+
+const atualizarNumeroItens = () => {
+    numeroItens.style.display = cart.length ? 'block' : 'none'
+    numeroItens.innerHTML = cart.length
+}
+
+//PAGE HOME
 
 
 // Ocultar o botão voltar e seção de detalhes do produto
