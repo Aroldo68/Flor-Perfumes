7551a6d4ae9ad56e9eec4f3fedb4b26be39c83ba
Aroldo68 <k21ajo@gmail.com>
2025-03-02 22:34:09 -0300

    Novas atualizações

--- js/script.js
+++ js/script.js
@@ -1,17 +1,20 @@
 const botaoVoltar = document.querySelector('.voltar')
 const sectionDetalhesProduto = document.querySelector('.produto__detalhes')
 const sectionProdutos = document.querySelector('.produtos')
-// Ocultar o botao detalhes e a seção de detalhes do produtos
-botaoVoltar.style.display = 'none'
-sectionDetalhesProduto.style.display = 'none'   
 
-const formatCurrency = (number) => {
-    return number.toLocaleString('pt-BR', {
+const ocultarBotaoEsecao = () => {
+    botaoVoltar.style.display = 'none'
+    sectionDetalhesProduto.style.display = 'none'   
+}
+
+ocultarBotaoEsecao()    
+
+const numberFormat = new Intl.NumberFormat('pt-BR', { 
         style: 'currency',
         currency: 'BRL',
-    })
-
-}
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2,
+    }) 
 
 const getProducts = async () => {
     const response = await fetch('js/products.json')
@@ -33,25 +36,11 @@ const generateCard = async () => {
                 <h4>${product.product_name}</h4>
                 <h5>${product.product_model}</h5>
             </div>
-            <h6>${formatCurrency(product.price)}</h6>
+            <h6>${numberFormat.format(product.price)}</h6>
         `
         const listaProdutos = document.querySelector('.lista__produtos')
         listaProdutos.appendChild(card)
-
-        card.addEventListener('click', (e) => {
-            // Ocultar e Mostrar o botão e a area de detalhes do produto
-            sectionProdutos.style.display = 'none'
-            botaoVoltar.style.display = 'block'
-            sectionDetalhesProduto.style.display = 'grid'
-
-            //Identificar qual o card foi clicado
-            const cardClicado = e.currentTarget
-            const idProduto = cardClicado.id
-            const produtoClicado = products.find( product => product.id == idProduto )
-            //console.log(produtoClicado)
-            //Preencher os dados de detalhes do produto
-            preencherDadosProduto(produtoClicado)
-        })
+        preencherCard(card, products)
     })
 }
 
@@ -59,8 +48,7 @@ generateCard()
 
 botaoVoltar.addEventListener('click', () => {
         sectionProdutos.style.display = 'flex'
-        botaoVoltar.style.display = 'none'
-        sectionDetalhesProduto.style.display = 'none'
+        ocultarBotaoEsecao()
 })
 
 const preencherDadosProduto = (product) => {
@@ -75,4 +63,30 @@ const preencherDadosProduto = (product) => {
     document.querySelector('.detalhes h4').innerHTML = product.product_name
     document.querySelector('.detalhes h5').innerHTML = product.product_model
     document.querySelector('.detalhes h6').innerHTML = numberFormat.format(product.price)
+}
+
+const details = document.querySelector('details')
+
+details.addEventListener('toggle', () => {
+    const summary = document.querySelector('summary')
+    summary.classList.toggle('icone-expandir')
+    summary.classList.toggle('icone-recolher')
+})
+
+//Preencher Card
+const preencherCard = (card, products) => {
+    card.addEventListener('click', (e) => {
+        // Ocultar e Mostrar o botão e a area de detalhes do produto
+        sectionProdutos.style.display = 'none'
+        botaoVoltar.style.display = 'block'
+        sectionDetalhesProduto.style.display = 'grid'
+
+        //Identificar qual o card foi clicado
+        const cardClicado = e.currentTarget
+        const idProduto = cardClicado.id
+        const produtoClicado = products.find( product => product.id == idProduto )
+        
+        //Preencher os dados de detalhes do produto
+        preencherDadosProduto(produtoClicado)
+    })
 }
