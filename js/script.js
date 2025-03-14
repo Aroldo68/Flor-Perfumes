4ffa3eb0265c99a1eb7743826f77e703c2a1d5af
Aroldo68 <aroldo.j.68@gmail.com>
2025-03-14 20:21:17 -0300

    Validação do Formulário

--- js/script.js
+++ js/script.js
@@ -269,72 +269,66 @@ const pegarDados = () => {
     return dados
 }
 
-const validacaoDoFormulario = () =>{
+const validacaoDoFormulario = () => {
+    let formularioValido = true
     todosCamposObrigatorios.forEach( campoObrigatorio => {
         const isEmpty = campoObrigatorio.value.trim() === ''
         const isNotChecked = campoObrigatorio.type === 'checkbox' && !campoObrigatorio.checked
-
         if(isEmpty) {
-            campoObrigatorio.nextElementSibling.textContent = `${campoObrigatorio.id} obrigatorio`
             campoObrigatorio.classList.add('campo-invalido')
-        }else{
-            campoObrigatorio.nextElementSibling.textContent = ``
+            campoObrigatorio.nextElementSibling.textContent = `${campoObrigatorio.id} obrigatorio`
+            formularioValido = false
+        } else {
             campoObrigatorio.classList.add('campo-valido')
             campoObrigatorio.classList.remove('campo-invalido')
+            campoObrigatorio.nextElementSibling.textContent = ''
         }
-
         if(isNotChecked) {
             campoObrigatorio.parentElement.classList.add('erro')
-        }else{
+            formularioValido = false
+        } else {
             campoObrigatorio.parentElement.classList.remove('erro')
         }
     })
+    return formularioValido
 }
 
 const btnFinalizarCadastro = document.querySelector('.btn_finalizar_cadastro')
 btnFinalizarCadastro.addEventListener('click', (event) => {
-    
     event.preventDefault()
-    
-    // validacoes
     validacaoDoFormulario()
-
-    // pegar dados
-    console.log(pegarDados)
-    
-})  
+    if(validacaoDoFormulario()) {
+        localStorage.setItem('dados', JSON.stringify(pegarDados()))
+        formularioIdentificacao.reset()
+        ocultarElemento(sectionIdentificacao)
+        mostrarElemento(sectionPagamento)
+    }
+})
 
 // Validação onBlur
 todosCamposObrigatorios.forEach( campo => {
-
     const emailRegex = /\S+@\S+\.\S+/
-
     campo.addEventListener('blur', (e) => {
-
         if(campo.value !== "" && e.target.type !== "email") {
             campo.classList.add('campo-valido')
             campo.classList.remove('campo-invalido')
             campo.nextElementSibling.textContent = ''
-        } else {    
+        } else {
             campo.classList.add('campo-invalido')
             campo.classList.remove('campo-valido')
             campo.nextElementSibling.textContent = `${campo.id} é obrigatório`
         }
-
         if(emailRegex.test(e.target.value)) {
             campo.classList.add('campo-valido')
             campo.classList.remove('campo-invalido')
             campo.nextElementSibling.textContent = ''
         }
-
         if(e.target.type === "checkbox" && !e.target.checked) {
             campo.parentElement.classList.add('erro')
         } else {
             campo.parentElement.classList.remove('erro')
         }
-
     })
-
 })
 
 const btnFinalizarCompra = document.querySelector('.btn_finalizar_compra')
